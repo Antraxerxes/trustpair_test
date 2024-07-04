54be2a89559c74324286a6d8dba686f62595f6c9
mtrijasse <marvin.trijasse@ingenico.com>
2024-07-04 10:31:52 +0200

    update VAT

--- trustin.rb
+++ trustin.rb
@@ -1,60 +1,73 @@
 require "json"
 require "net/http"
 
+require 'net/http'
+require 'json'
+require 'uri'
+
 class TrustIn
   def initialize(evaluations)
     @evaluations = evaluations
   end
 
-  def update_score()
+  def update_score
     @evaluations.each do |evaluation|
-      if evaluation.type == "SIREN"
-        if evaluation.score > 0 && evaluation.state == "unconfirmed" && evaluation.reason == "ongoing_database_update"
-          uri = URI("https://public.opendatasoft.com/api/records/1.0/search/?dataset=economicref-france-sirene-v3" \
-            "&q=#{evaluation.value}&sort=datederniertraitementetablissement" \
-            "&refine.etablissementsiege=oui")
-          response = Net::HTTP.get(uri)
-          parsed_response = JSON.parse(response)
-          company_state = parsed_response["records"].first["fields"]["etatadministratifetablissement"]
-          if company_state == "Actif"
-            evaluation.state = "favorable"
-            evaluation.reason = "company_opened"
-            evaluation.score = 100
-          else
-            evaluation.state = "unfavorable"
-            evaluation.reason = "company_closed"
-            evaluation.score = 100
-          end
+      if evaluation.type == "SIREN" || evaluation.type == "VAT"
+        if evaluation_needs_api_update?(evaluation)
+          company_state = fetch_company_state(evaluation.value)
+          update_evaluation_based_on_state(evaluation, company_state)
         elsif evaluation.score >= 50
-          if evaluation.state == "unconfirmed" && evaluation.reason == "unable_to_reach_api"
-            evaluation.score = evaluation.score - 5
-          elsif evaluation.state == "favorable"
-            evaluation.score = evaluation.score - 1
-          end
-        elsif evaluation.score <= 50 && evaluation.score > 0
-          if evaluation.state == "unconfirmed" && evaluation.reason == "unable_to_reach_api" || evaluation.state == "favorable"
-            evaluation.score = evaluation.score - 1
-          end
-        else
-          if evaluation.state == "favorable" || evaluation.state == "unconfirmed"
-            uri = URI("https://public.opendatasoft.com/api/records/1.0/search/?dataset=economicref-france-sirene-v3" \
-                      "&q=#{evaluation.value}&sort=datederniertraitementetablissement" \
-                      "&refine.etablissementsiege=oui")
-            response = Net::HTTP.get(uri)
-            parsed_response = JSON.parse(response)
-            company_state = parsed_response["records"].first["fields"]["etatadministratifetablissement"]
-            if company_state == "Actif"
-              evaluation.state = "favorable"
-              evaluation.reason = "company_opened"
-              evaluation.score = 100
-            else
-              evaluation.state = "unfavorable"
-              evaluation.reason = "company_closed"
-              evaluation.score = 100
-            end
-          end
+          adjust_score_for_high_score(evaluation)
+        elsif evaluation.score > 0
+          adjust_score_for_low_score(evaluation)
         end
       end
+    end
+  end
+
+  private
+
+  def evaluation_needs_api_update?(evaluation)
+    evaluation.state == "unconfirmed" && evaluation.reason == "ongoing_database_update" && evaluation.score > 0
+  end
+
+  def fetch_company_state(siren_value)
+    if evaluation.type == "SIREN"
+      uri = URI("https://public.opendatasoft.com/api/records/1.0/search/?dataset=economicref-france-sirene-v3&q=#{siren_value}&sort=datederniertraitementetablissement&refine.etablissementsiege=oui")
+      response = Net::HTTP.get(uri)
+      parsed_response = JSON.parse(response)
+      parsed_response["records"].first["fields"]["etatadministratifetablissement"]
+    else 
+      #just dont move value for vat. could use a stub 
+      evaluation.state
+  end
+
+  def update_evaluation_based_on_state(evaluation, company_state)
+    if company_state == "Actif"
+      evaluation.state = "favorable"
+      evaluation.reason = "company_opened"
+      evaluation.score = 100
+    else
+      evaluation.state = "unfavorable"
+      evaluation.reason = "company_closed"
+      evaluation.score = 100
+    end
+  end
+
+  def adjust_score_for_high_score(evaluation)
+    if evaluation.state == "unconfirmed" && evaluation.reason == "unable_to_reach_api"
+      adjustment = evaluation.type == "SIREN" ? 5 : 1
+      evaluation.score -= adjustment
+    elsif evaluation.state == "favorable"
+      adjustment = evaluation.type == "SIREN" ? 1 : 3
+      evaluation.score -= adjustment
+    end
+  end
+
+  def adjust_score_for_low_score(evaluation)
+    if (evaluation.state == "unconfirmed" && evaluation.reason == "unable_to_reach_api") || evaluation.state == "favorable"
+      adjustment = evaluation.type == "SIREN" ? 1 : 3
+      evaluation.score -= adjustment
     end
   end
 end
--- trustin_spec.rb
+++ trustin_spec.rb
@@ -67,5 +67,67 @@ RSpec.describe TrustIn do
         end
       end
     end
+
+    context "when the evaluation type is 'VAT'" do
+      context "with a <score> greater or equal to 50 AND the <state> is unconfirmed and the <reason> is 'unable_to_reach_api'" do
+        let(:evaluations) { [Evaluation.new(type: "VAT", value: "123456789", score: 79, state: "unconfirmed", reason: "unable_to_reach_api")] }
+
+        it "decreases the <score> of 1" do
+          expect(evaluations.first.score).to eq(78)
+        end
+      end
+
+      context "when the <state> is unconfirmed and the <reason> is 'unable_to_reach_api'" do
+        let(:evaluations) { [Evaluation.new(type: "VAT", value: "123456789", score: 37, state: "unconfirmed", reason: "unable_to_reach_api")] }
+
+        it "decreases the <score> of 3" do
+          expect(evaluations.first.score).to eq(34)
+        end
+      end
+
+      context "when the <state> is favorable" do
+        let(:evaluations) { [Evaluation.new(type: "VAT", value: "123456789", score: 28, state: "favorable", reason: "company_opened")] }
+
+        it "decreases the <score> of 3" do
+          expect(evaluations.first.score).to eq(25)
+        end
+      end
+
+      context "when the <state> is 'unconfirmed' AND the <reason> is 'ongoing_database_update'" do
+        let(:evaluations) { [Evaluation.new(type: "VAT", value: "IE6388047V", score: 42, state: "unconfirmed", reason: "ongoing_database_update")] }
+
+        it "assigns a <state> and a <reason> to the evaluation based on the API response and a <score> to 100" do
+          expect(evaluations.first.state).to eq("favorable")
+          expect(evaluations.first.reason).to eq("company_opened")
+          expect(evaluations.first.score).to eq(100)
+        end
+      end
+
+      context "with a <score> equal to 0" do
+        let(:evaluations) { [Evaluation.new(type: "VAT", value: "LU26375245", score: 0, state: "favorable", reason: "company_opened")] }
+
+        it "assigns a <state> and a <reason> to the evaluation based on the API response and a <score> to 100" do
+          expect(evaluations.first.state).to eq("unfavorable")
+          expect(evaluations.first.reason).to eq("company_closed")
+          expect(evaluations.first.score).to eq(100)
+        end
+      end
+
+      context "with a <state> 'unfavorable'" do
+        let(:evaluations) { [Evaluation.new(type: "VAT", value: "123456789", score: 52, state: "unfavorable", reason: "company_closed")] }
+
+        it "does not decrease its <score>" do
+          expect { subject }.not_to change { evaluations.first.score }
+        end
+      end
+
+      context "with a <state>'unfavorable' AND a <score> equal to 0" do
+        let(:evaluations) { [Evaluation.new(type: "VAT", value: "123456789", score: 0, state: "unfavorable", reason: "company_closed")] }
+
+        it "does not call the API" do
+          expect(Net::HTTP).not_to receive(:get)
+        end
+      end
+    end
   end
 end
